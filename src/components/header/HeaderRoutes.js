61e23477d24a1e52603ed0685022e2be6ffd5184
Omar Cardenas <omarenriquect@gmail.com>
2020-11-28 10:11:44 -0500

    adjusting details

--- src/components/header/HeaderRoutes.js
+++ src/components/header/HeaderRoutes.js
@@ -20,7 +20,7 @@ const MenuComponent = () => {
                 onMouseLeave={hideDropdown}
                 >
                 {navigationDropdownConfig.map((dropItem, i) => (
-                    <NavItem linkTo={`${dropItem.path}`}>{dropItem.name}</NavItem>
+                    <NavItem key={i} linkTo={`${dropItem.path}`}>{dropItem.name}</NavItem>
                 ))}
             </NavDropdown>
 
